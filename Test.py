5022ecfeed918c78bcaa49db45b402de5c6647ba
Vimmi Swami <vimmi.swami@outlook.com>
2018-06-21 18:27:09 -0700

    modified test to replace ip with localhost

--- Test.py
+++ Test.py
@@ -5,11 +5,15 @@ passed=0
 failed =0
 total= 0
 
+ping = 'http://localhost:3000/pingLB'
+add = "http://localhost:3000/add_temperature_measurement"
+get_measurement = "http://localhost:3000/get_temperature_measurements"
+get_avg = "http://localhost:3000/get_average_temperature"
 
 #Test Ping Request
 print("Runnung Test Ping Request...")
 total=total+1
-response = requests.get('http://localhost:3000/pingLB')
+response = requests.get(ping)
 json_data1 = json.loads(response.text)
 print("Response: \n")
 print(json_data1["Ping"])
@@ -24,7 +28,6 @@ print("-----------------------------------------------\n")
 #Test Add Measurement Request-Sucess1
 print("Running Test Add Measurement Request-Sucess1..." )
 total=total+1
-url = "http://localhost:3000/add_temperature_measurement"
 
 payload = "{\n\t\"Timestamp\":\"2018-05-16T15:04:05.000Z\",\n\t\"Tempt\":\"5.2\"\n}"
 headers = {
@@ -32,7 +35,7 @@ headers = {
     'Cache-Control': "no-cache",
     }
 
-response = requests.request("POST", url, data=payload, headers=headers)
+response = requests.request("POST", add, data=payload, headers=headers)
 json_data2 = json.loads(response.text)
 print("Response: \n")
 print(json_data2)
@@ -48,7 +51,7 @@ print("-----------------------------------------------\n")
 print("Running Test Add Measurement Request-Sucess2.. ")
 total=total+1
 payload = "{\n\t\"Timestamp\":\"2018-04-10T15:04:05.000Z\",\n\t\"Tempt\":\"2.5\"\n}"
-response = requests.request("POST", url, data=payload, headers=headers)
+response = requests.request("POST", add, data=payload, headers=headers)
 json_data22 = json.loads(response.text)
 print("Response: \n")
 print(json_data22)
@@ -65,7 +68,7 @@ print("Running Test Add Measurement Request-Invalid timestamp...")
 total=total+1
 payload = "{\n\t\"Timestamp\":\"abcd\",\n\t\"Tempt\":\"2.2\"\n}"
 
-responseq = requests.request("POST", url, data=payload, headers=headers)
+responseq = requests.request("POST", add, data=payload, headers=headers)
 print("Response: \n")
 print(responseq.text, responseq.status_code)
 if ("Null or Invalid timestamp, expected: 2006-01-02T15:04:05.000Z  format" in responseq.text) and (responseq.status_code == 400 ) :
@@ -81,7 +84,7 @@ print("Running Test Add Measurement Request-Invalid Temperature...")
 total=total+1
 payload = "{\n\t\"Timestamp\":\"2018-07-16T15:04:05.000Z\",\n\t\"Tempt\":\"sdf\"\n}"
 
-responsea = requests.request("POST", url, data=payload, headers=headers)
+responsea = requests.request("POST", add, data=payload, headers=headers)
 print("Response: \n")
 print(responsea.text)
 if ( "Null or Invalid temperature or out-of-bounds"  in responsea.text) and (responsea.status_code == 400 ) :
@@ -97,7 +100,7 @@ print("Running Test Add Measurement Request-Missing Body...")
 total=total+1
 payloadd = ""
 
-responses = requests.request("POST", url, data=payloadd, headers=headers)
+responses = requests.request("POST", add, data=payloadd, headers=headers)
 print("Response: \n")
 print(responses.text)
 if ("Null or Invalid timestamp, expected: 2006-01-02T15:04:05.000Z  format" in responses.text) and (responses.status_code == 400 ) :
@@ -111,13 +114,12 @@ else:
 #Test Get Measurement Range Request-Success
 print("Running Test Get Measurement Range Request-Success...")
 total=total+1
-url = "http://localhost:3000/get_temperature_measurements"
 querystring = {"start_timestamp":"2018-04-10T15:04:05.000Z","stop_timestamp":"2018-06-10T15:04:05.000Z"} #includes the two added measurement
 headers = {
     'Cache-Control': "no-cache",
     }
 
-responsez = requests.request("GET", url, headers=headers, params=querystring)
+responsez = requests.request("GET", get_measurement, headers=headers, params=querystring)
 json_data3 = json.loads(responsez.text)
 print("Response: \n")
 print(json_data3)
@@ -134,7 +136,7 @@ print("-----------------------------------------------\n")
 print("Running Test Get Measurement Range Request-Missing param...")
 total=total+1
 querystring = {"stop_timestamp":"2018-06-10T15:04:05.000Z"}#missing start_timestamp
-responsew = requests.request("GET", url, headers=headers, params=querystring)
+responsew = requests.request("GET", get_measurement, headers=headers, params=querystring)
 print("Response: \n")
 print(responsew.text)
 
@@ -150,7 +152,7 @@ print("-----------------------------------------------\n")
 print("Running Test Get Measurement Range Request-Incorrect Timestamp...")
 total=total+1
 querystring = {"start_timestamp":"abcd","stop_timestamp":"2018-06-10T15:04:05.000Z"}
-responsex = requests.request("GET", url, headers=headers, params=querystring)
+responsex = requests.request("GET", get_measurement, headers=headers, params=querystring)
 print("Response: \n")
 print(responsex.text)
 
@@ -166,7 +168,7 @@ print("-----------------------------------------------\n")
 print("Running Test Get Measurement Range Request-No result...")
 total=total+1
 querystring = {"start_timestamp":"2013-06-10T15:04:05.000Z","stop_timestamp":"2013-06-10T15:04:05.000Z"} #this range has no data in db
-response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", get_measurement, headers=headers, params=querystring)
 print("Response: \n")
 print(response.text)
 
@@ -181,9 +183,8 @@ print("-----------------------------------------------\n")
 #Test Get Average Temp Range Request-Sucess
 print("Running Test Get Average Temp Range Request-Sucess...")
 total=total+1	
-url = "http://localhost:3000/get_average_temperature"
 querystring = {"start_timestamp":"2018-03-15T15:04:05.000Z","stop_timestamp":"2018-10-20T15:04:05.000Z"}#all inclusive
-response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", get_avg, headers=headers, params=querystring)
 print("Response: \n")
 print(response.text)
 
@@ -199,9 +200,9 @@ print("-----------------------------------------------\n")
 #Test Get Average Temp Range Request-Out-of-range
 print("Running Test Get Average Temp Range Request-Out-of-range...")
 total=total+1	
-url = "http://localhost:3000/get_average_temperature"
+
 querystring = {"start_timestamp":"2012-05-15T15:04:05.000Z","stop_timestamp":"2012-05-20T15:04:05.000Z"}
-response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", get_avg, headers=headers, params=querystring)
 print("Response: \n")
 print(response.text)
 
